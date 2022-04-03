8e2128d80fe042bfe6435d5d84e7ad90dc78d5fe
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-04-03 18:28:30 -0400

    Created html for Signup page including a form for signing up

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,9 +1,34 @@
 import './Signup.scss';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import apiBaseUrl from '../../config/config';
+
 
 const Signup = (routerProps) => {
 
-    return(
-        <h1>Signup</h1>
+    return (
+
+        <div className='signup'>
+
+            <h1>Signup</h1>
+
+            <form >
+
+                <label className='form-detail_label' htmlFor='name'>Name: </label>
+                <input className='form-detail_input' name='name' type="text" placeholder='' />
+
+                <label className='form-detail_label' htmlFor='username'>Username: </label>
+                <input className='form-detail_input' name='username' type="text" placeholder='' />
+
+                <label className='form-detail_label' htmlFor='password'>Password: </label>
+                <input className='form-detail_input' name='password' type="password" placeholder='' />
+
+                <button type="submit">Signup</button>
+            </form>
+
+            <Link to='/signup'>Login</Link>
+
+        </div>
     );
 };
 
