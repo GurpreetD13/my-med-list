575926f64ee4c6c33d60c5b2850bbd6cb55b8da5
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-04-03 19:06:16 -0400

    Created handleSignup function to handle new user signup form onSubmit that sends a POST request to API signup endpoint to create a new user in database, and tested it

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -6,13 +6,29 @@ import apiBaseUrl from '../../config/config';
 
 const Signup = (routerProps) => {
 
+    const handleSignup = (e) => {
+        e.preventDefault();
+        // send a POST request with new user data from signup form to API signup endpoint which will create a new user
+        const newUser = {
+            name: e.target.name.value,
+            user_name: e.target.username.value,
+            password: e.target.password.value
+        }
+        axios.post(`${apiBaseUrl}/users/signup`, newUser)
+            .then(res => {
+                // Redirect to Login page ('/login') after signed up, using history routerProps being passed down
+                routerProps.history.push('/login');
+            })
+            .catch(err => console.log(err));
+    };
+
     return (
 
         <div className='signup'>
 
             <h1>Signup</h1>
 
-            <form >
+            <form onSubmit={handleSignup}>
 
                 <label className='form-detail_label' htmlFor='name'>Name: </label>
                 <input className='form-detail_input' name='name' type="text" placeholder='' />
