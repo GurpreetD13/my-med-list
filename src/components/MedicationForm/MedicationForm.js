b1120f4dd306f21d6218f8532bf999465a6ddc33
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-04-04 22:55:58 -0400

    Created handleSubmit function to capture form inputs into a newMedication object

--- src/components/MedicationForm/MedicationForm.js
+++ src/components/MedicationForm/MedicationForm.js
@@ -1,18 +1,61 @@
 import './MedicationForm.scss';
 import React from 'react';
 import axios from 'axios';
+import apiBaseUrl from '../../config/config';
 
 
 class MedicationForm extends React.Component {
 
-    // initialize state to emtpy form fields
+    // initialize state to empty form fields
     state = {
         din: '',
         medication: '',
         instructions: '',
     };
 
+    handleSubmit = (e) => {
+        e.preventDefault();
+        // only if form is valid
+        // if (this.isFormValid()) {
+            // create newMedication to send to API server based on user's inputs in form
+            // have to change e.target.inputName.value to this.state.inputName
+            const newMedication = {
+                din: this.state.din,
+                // medication: this.state.medication,
+                instructions: this.state.instructions,
 
+            };
+            // send newMedication to API server
+            // axios.post(`${apiBaseUrl}/medications`, newMedication)
+            //     .then(res => {
+            //         // Redirect to new Warehouse page ('/warehouses/newWarehouseId') after form submitted, have to use this.props since this is a class
+            //         // this.props.routerProps.history.push(`/warehouses/${res.data.id}`);
+            //     })
+            //     .catch(error => console.log(error));
+
+            // reset state to empty form fields after page submission 
+            // this.setState({
+            //     din: '',
+            //     medication: '',
+            //     instructions: '',
+            // });
+        // }
+    }
+
+    // handleChange handler function changes/controls state of stored form input values and is applied to each input field
+    // handleChange = (event) => {
+    //     this.setState({
+    //         [event.target.name]: event.target.value
+    //     });
+    // };
+
+    // handler function to disable form submission if form is invalid (i.e. din not 8 or no proper instructions)
+    // isFormValid = () => {
+    //     if (this.state.din.length() !== 8 || this.state.instructions.length() < 8) {
+    //         return false;
+    //     }
+    //     return true;
+    // };
 
 
     render() {
