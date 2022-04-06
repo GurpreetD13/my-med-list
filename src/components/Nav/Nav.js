41b17df090f85d6fe3255bffcb4201f4b67f096c
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-04-05 22:32:32 -0400

    Styled Nav component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,19 +1,16 @@
 import './Nav.scss';
 import { NavLink } from 'react-router-dom';
+import logo from '../../assets/logos/heart-pulse.svg'
 
 
 const Nav = () => {
 
     return (
         <nav className='nav'>
-            <NavLink to='/'>
-
-                
-                <p className='nav__logo'>My Med List</p>
+            <NavLink to='/' className='nav__logo'>
+                <img src={logo} alt='logo for My Med List' />
+                <p> My Med List</p>
             </NavLink>
-
-
-
         </nav>
     );
 };
